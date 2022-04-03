10f931ff328abb8050fa2d46a08975b2fe18b1bb
Lucas Towers <luctowers@gmail.com>
2022-04-02 23:09:47 -0700

    EVEN MORE COMMENTS

--- src/main/java/ubc/cosc322/engine/core/Move.java
+++ src/main/java/ubc/cosc322/engine/core/Move.java
@@ -1,15 +1,24 @@
 package ubc.cosc322.engine.core;
 
+/** 
+ * Move are represented by integers, so this class is not constructable.
+ * But it contains methods to decode/encode move information.
+ **/
 public class Move {
-	
+
+	private Move() {}
+
+	/** Packs two positions into single int for the queen move. */
 	public static int encodeQueenMove(int queenSource, int queenDestination) {
 		return (queenDestination << 16) | queenSource;
 	}
 
+	/** Unpacks the queen source from a queen move. */
 	public static int decodeQueenSource(int queenMove) {
 		return queenMove & 0xFFFF;
 	}
 
+	/** Unpacks the queen destination from a queen move. */
 	public static int decodeQueenDestination(int queenMove) {
 		return queenMove >> 16;
 	}
--- src/main/java/ubc/cosc322/engine/core/MoveType.java
+++ src/main/java/ubc/cosc322/engine/core/MoveType.java
@@ -1,11 +1,17 @@
 package ubc.cosc322.engine.core;
 
+/**
+ * In our implemenation, a turn is split into two moves, a queen move, and an
+ * arrow move. We previously also consided the queen pick to e third move type.
+ */
 public enum MoveType {
 
 	QUEEN, ARROW;
 
+	/** The number of move types. */
 	public static final int COUNT = 2;
 
+	/** The next move type in the sequence. */
 	public MoveType next() {
 		switch (this) {
 			case QUEEN:
@@ -16,6 +22,7 @@ public enum MoveType {
 		return null;
 	}
 
+	/** The previous move type in the sequence. */
 	public MoveType previous() {
 		switch (this) {
 			case QUEEN:
